67b45e1c4c485f1aa67866cb59aa54db4893a4bf
ctinray <b03902072@ntu.edu.tw>
2018-01-07 23:00:56 +0800

    wrote _renorm_r

--- src/capsule.py
+++ src/capsule.py
@@ -163,11 +163,102 @@ def conv_em_routing(activation, conv_votes, stride, routing_iters):
 
 
 def _renorm_r(r, stride):
-    pass
+    """Renorm r for each capsule in lower layer, its contribution to upper
+    layer capsules sum to 1.
+
+    Args:
+        r (tensor): Expected portion of lower capsule that belong to
+            the upper capsule.
+            Shape: [batch, output_height, output_width, channels_out,
+                    kernel_size ** 2, channels_in]
+        stride (int): Stride of the convolution layer before routing.
+    """
+    kernel_size = math.sqrt(r.shape[-2])
+    assert kernel_size.is_integer()
+    kernel_size = int(kernel_size)
+
+    origin_h = r.shape[1] * stride + (kernel_size - 1)
+    origin_w = r.shape[2] * stride + (kernel_size - 1)
+
+    channels_out = r.shape[3]
+    channels_in = r.shape[-1]
+
+    # collect indices of higher level units that convolve lower level
+    # unit at i, j.
+    higher_indices = [[[] for w in range(origin_w)]
+                      for h in range(origin_h)]
+    for i in range(r.shape[1]):
+        for j in range(r.shape[2]):
+            for ki in range(kernel_size):
+                for kj in range(kernel_size):
+                    higher_indices[i + ki][j + kj].append((i, j))
+
+    # the max number of upper units that convolve a lower unit
+    max_convolved = max([max(map(len, arr)) for arr in higher_indices])
+
+    # flattern upper level n-dim indices to 1-dim
+    sum_r_indices = []
+    for i in range(origin_h):
+        for j in range(origin_w):
+            for cout in channels_out:
+                for k in range(max_convolved):
+                    for cin in channels_in:
+                        if k < len(higher_indices[i][j]):
+                            higher_i, higher_j = higher_indices[i][j]
+                            index = ((((higher_i * r.shape[1] + higher_j)
+                                       * channels_out + cout)
+                                      * kernel_size ** 2 + k)
+                                     * channels_in + cin)
+                            sum_r_indices.append(index)
+                        else:
+                            # for border units that are convolved less times
+                            # append padding index that points to 0
+                            sum_r_indices.append(-1)
+
+    # keep only the batch dimension so we can use tf.gather easily
+    r = r.reshape(r.shape[0], -1)
+
+    # pad r with 0 so index -1 will point to 0
+    r = tf.cat([r, tf.zeros([r.shape[0], 1])], axis=-1)
+
+    # gather r that is contributed from lower layer unit i, j
+    r_gather = tf.gather(r, sum_r_indices, axis=-1)
+    r_gather = tf.reshape(r_gather, [-1,
+                                     origin_h,
+                                     origin_w,
+                                     channels_out * max_convolved,
+                                     channels_in])
+
+    # summation of r
+    r_sum = tf.reduce_sum(r_gather, -2, keep_dims=True)
+
+    # collect r_sum
+    conv_r_sum = tf.extract_image_paches(
+        r_sum,
+        [1, kernel_size, kernel_size, 1],
+        [1, stride, stride, 1],
+        [1, 1, 1, 1],
+        'VALID')
+
+    # calculate summation of r
+    conv_r_sum = tf.reshape(
+        conv_r_sum,
+        [r.shape[0],        # batch
+         r.shape[1],        # output_height
+         r.shape[2],        # output_width
+         kernel_size ** 2,
+         channels_in])
+
+    # renorm r by divide original r with conv_r_sum
+    conv_r_renormed = r / tf.expand_dims(conv_r_sum, 3)
+    assert conv_r_renormed.shape == r
+
+    return conv_r_renormed
 
 
 def _conv_e_step(m, s, a_prime, v, stride):
-    """E-step of the EM algorithm
+    """E-step of the EM algorithm.
+    Note that only VALID padding is supported (when renorming r).
 
     Args:
         m (tensor): Pose of capsules.
