5ee91af3dd992e113a9ca028e807385346b9f481
ctinray <b03902072@ntu.edu.tw>
2018-01-15 11:02:39 +0800

    script to plot curves

---
+++ scripts/plot.py
@@ -0,0 +1,54 @@
+import pdb
+import argparse
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+def read_file(filename):
+    xs = []
+    ys = []
+    with open(filename) as f:
+        for l in f:
+            cols = l.split(',')
+            xs.append(float(cols[0]))
+            ys.append(float(cols[1]))
+
+    return xs, ys
+
+
+def main(args):
+    fig = plt.figure(dpi=450)
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_xlabel(args.x_title)
+    ax.set_ylabel(args.y_title)
+
+    ax.set_ylim([0, 1])
+    ax.grid()
+
+    for i in range(len(args.log_file)):
+        xs, ys = read_file(args.log_file[i])
+
+        label = args.labels[i] if args.labels is not None else None
+        ax.plot(xs, ys,
+                label=label, linewidth=2.0)
+
+    ax.legend()
+    fig.savefig(args.output)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Plot learning curve.')
+    parser.add_argument('log_file', type=str, nargs='+',
+                        help='Path to the log file.')
+    parser.add_argument('output', type=str,
+                        help='Directory of the data.')
+    parser.add_argument('--labels', type=str, nargs='+', default=None)
+    parser.add_argument('--x_title', type=str, default='')
+    parser.add_argument('--y_title', type=str, default='')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
